9b341e8a7b3c14f07c96e592f2762c3de449273b
linpaul2004 <linpaul2004@gmail.com>
2016-08-06 01:45:53 +0800

    Basic Gaming

---
+++ Chess/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Chess
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+		/// 修改這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.chessIcon = new System.Windows.Forms.ImageList(this.components);
+			this.whiteTurn = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.blackTurn = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.whiteClock = new System.Windows.Forms.Label();
+			this.blackClock = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// chessIcon
+			// 
+			this.chessIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("chessIcon.ImageStream")));
+			this.chessIcon.TransparentColor = System.Drawing.Color.Transparent;
+			this.chessIcon.Images.SetKeyName(0, "BlackBishop.png");
+			this.chessIcon.Images.SetKeyName(1, "BlackKing.png");
+			this.chessIcon.Images.SetKeyName(2, "BlackKnight.png");
+			this.chessIcon.Images.SetKeyName(3, "BlackPawn.png");
+			this.chessIcon.Images.SetKeyName(4, "BlackQueen.png");
+			this.chessIcon.Images.SetKeyName(5, "BlackRook.png");
+			this.chessIcon.Images.SetKeyName(6, "WhiteBishop.png");
+			this.chessIcon.Images.SetKeyName(7, "WhiteKing.png");
+			this.chessIcon.Images.SetKeyName(8, "WhiteKnight.png");
+			this.chessIcon.Images.SetKeyName(9, "WhitePawn.png");
+			this.chessIcon.Images.SetKeyName(10, "WhiteQueen.png");
+			this.chessIcon.Images.SetKeyName(11, "WhiteRook.png");
+			// 
+			// whiteTurn
+			// 
+			this.whiteTurn.AutoSize = true;
+			this.whiteTurn.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.whiteTurn.Location = new System.Drawing.Point(339, 299);
+			this.whiteTurn.Name = "whiteTurn";
+			this.whiteTurn.Size = new System.Drawing.Size(136, 32);
+			this.whiteTurn.TabIndex = 0;
+			this.whiteTurn.Text = "WHITE turn";
+			// 
+			// label2
+			// 
+			this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label2.Location = new System.Drawing.Point(338, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(2, 380);
+			this.label2.TabIndex = 1;
+			// 
+			// blackTurn
+			// 
+			this.blackTurn.AutoSize = true;
+			this.blackTurn.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.blackTurn.Location = new System.Drawing.Point(339, 9);
+			this.blackTurn.Name = "blackTurn";
+			this.blackTurn.Size = new System.Drawing.Size(135, 32);
+			this.blackTurn.TabIndex = 2;
+			this.blackTurn.Text = "BLACK turn";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(345, 154);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(130, 35);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "START";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// whiteClock
+			// 
+			this.whiteClock.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.whiteClock.Location = new System.Drawing.Point(345, 41);
+			this.whiteClock.Name = "whiteClock";
+			this.whiteClock.Size = new System.Drawing.Size(130, 23);
+			this.whiteClock.TabIndex = 4;
+			this.whiteClock.Text = "0:0";
+			this.whiteClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// blackClock
+			// 
+			this.blackClock.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.blackClock.Location = new System.Drawing.Point(344, 277);
+			this.blackClock.Name = "blackClock";
+			this.blackClock.Size = new System.Drawing.Size(130, 23);
+			this.blackClock.TabIndex = 5;
+			this.blackClock.Text = "0:0";
+			this.blackClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 341);
+			this.Controls.Add(this.blackClock);
+			this.Controls.Add(this.whiteClock);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.blackTurn);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.whiteTurn);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ImageList chessIcon;
+		private System.Windows.Forms.Label whiteTurn;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label blackTurn;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label whiteClock;
+		private System.Windows.Forms.Label blackClock;
+		private System.Windows.Forms.Timer timer1;
+
+	}
+}
+
---
+++ Chess/Form1.cs
@@ -0,0 +1,558 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+	public partial class Form1 : Form
+	{
+		private CheckBox[] square = new CheckBox[64];
+		private uint white, black;
+		private player now;
+		private readonly int SQUARESIZE = 40;
+		private readonly Color DARK = Color.SlateGray, LIGHT = Color.WhiteSmoke, SEL = Color.Gold;
+		private enum piece { BK, WK, BQ, WQ, BN, WN, BB, WB, BR, WR, BP, WP, NA };
+		private enum player { WHITE, BLACK };
+		private piece[] board = new piece[64];
+		private int checkIndex;
+
+		public Form1()
+		{
+			InitializeComponent();
+			Text = "Chess";
+			white = black = 1800;
+			whiteTurn.Visible = false;
+			blackTurn.Visible = false;
+			whiteClock.Text = "" + white / 60 + ":" + white % 60;
+			blackClock.Text = "" + black / 60 + ":" + black % 60;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 64; i++)
+			{
+				square[i] = new CheckBox();
+				square[i].Size = new Size(SQUARESIZE, SQUARESIZE);
+				square[i].AutoSize = false;
+				square[i].Location = new Point(12 + (i % 8) * SQUARESIZE, 12 + (i / 8) * SQUARESIZE);
+				square[i].Appearance = Appearance.Button;
+				square[i].FlatStyle = FlatStyle.Flat;
+				square[i].Visible = true;
+				if (((i / 8) % 2 == 0 && i % 2 == 1) || ((i / 8) % 2 == 1 && i % 2 == 0))
+				{
+					square[i].BackColor = DARK;
+				}
+				else
+				{
+					square[i].BackColor = LIGHT;
+				}
+				square[i].ImageList = chessIcon;
+				square[i].Tag = i;
+				square[i].Enabled = false;
+				square[i].Click += BoardClick;
+				Controls.Add(square[i]);
+			}
+			GameSet();
+			RefreshBoard();
+		}
+
+		private void BoardClick(object sender, EventArgs e)
+		{
+			int index = int.Parse(((CheckBox)sender).Tag.ToString());
+			if (index == checkIndex)
+			{
+				square[index].Checked = false;
+				checkIndex = -1;
+			}
+			else
+			{
+				if ((board[index] == piece.NA) && (checkIndex == -1))
+				{
+					square[index].Checked = false;
+				}
+				else if ((board[index] != piece.NA) && (checkIndex == -1))
+				{
+					if ((now == player.WHITE && (int)board[index] % 2 == 1) || (now == player.BLACK && (int)board[index] % 2 == 0))
+					{
+						checkIndex = index;
+					}
+					else
+					{
+						square[index].Checked = false;
+					}
+				}
+				else if ((board[index] == piece.NA) && (checkIndex >= 0))
+				{
+					if (Movable(checkIndex, index))
+					{
+						board[index] = board[checkIndex];
+						board[checkIndex] = piece.NA;
+						now = now == player.WHITE ? player.BLACK : player.WHITE;
+						if (now == player.WHITE)
+						{
+							whiteTurn.Visible = true;
+							blackTurn.Visible = false;
+						}
+						else
+						{
+							whiteTurn.Visible = false;
+							blackTurn.Visible = true;
+						}
+					}
+					square[index].Checked = false;
+					square[checkIndex].Checked = false;
+					if (((checkIndex / 8) % 2 == 0 && checkIndex % 2 == 1) || ((checkIndex / 8) % 2 == 1 && checkIndex % 2 == 0))
+					{
+						square[checkIndex].BackColor = DARK;
+					}
+					else
+					{
+						square[checkIndex].BackColor = LIGHT;
+					}
+					checkIndex = -1;
+				}
+				else
+				{
+					if (!isSameColor(index, checkIndex))
+					{
+						if (Movable(checkIndex, index))
+						{
+							board[index] = board[checkIndex];
+							board[checkIndex] = piece.NA;
+							now = now == player.WHITE ? player.BLACK : player.WHITE;
+							if (now == player.WHITE)
+							{
+								whiteTurn.Visible = true;
+								blackTurn.Visible = false;
+							}
+							else
+							{
+								whiteTurn.Visible = false;
+								blackTurn.Visible = true;
+							}
+						}
+					}
+					square[index].Checked = false;
+					square[checkIndex].Checked = false;
+					if (((checkIndex / 8) % 2 == 0 && checkIndex % 2 == 1) || ((checkIndex / 8) % 2 == 1 && checkIndex % 2 == 0))
+					{
+						square[checkIndex].BackColor = DARK;
+					}
+					else
+					{
+						square[checkIndex].BackColor = LIGHT;
+					}
+					checkIndex = -1;
+				}
+			}
+			if (square[index].Checked)
+			{
+				((CheckBox)sender).BackColor = SEL;
+			}
+			else
+			{
+				if (((index / 8) % 2 == 0 && index % 2 == 1) || ((index / 8) % 2 == 1 && index % 2 == 0))
+				{
+					square[index].BackColor = DARK;
+				}
+				else
+				{
+					square[index].BackColor = LIGHT;
+				}
+			}
+			square[index].Focus();
+			RefreshBoard();
+		}
+
+		private bool isNotBlock(int from, int to, int fromRow, int fromCol, int toRow, int toCol, bool vertical, bool horizontal, bool diagonal)
+		{
+			switch (board[from])
+			{
+				case piece.BB:
+				case piece.WB:
+					for (int row = fromRow, col = fromCol; ; )
+					{
+						if (toRow - fromRow < 0)
+						{
+							row--;
+						}
+						else
+						{
+							row++;
+						}
+						if (toCol - fromCol < 0)
+						{
+							col--;
+						}
+						else
+						{
+							col++;
+						}
+						if (row == toRow || col == toCol)
+						{
+							return true;
+						}
+						if (board[row * 8 + col] != piece.NA)
+						{
+							return false;
+						}
+					}
+				case piece.BR:
+				case piece.WR:
+					if (vertical)
+					{
+						for (int row = fromRow; ; )
+						{
+							if (toRow - fromRow < 0)
+							{
+								row--;
+							}
+							else
+							{
+								row++;
+							}
+							if (row == toRow)
+							{
+								return true;
+							}
+							if (board[row * 8 + fromCol] != piece.NA)
+							{
+								return false;
+							}
+						}
+					}
+					else
+					{
+						for (int col = fromCol; ; )
+						{
+							if (toCol - fromCol < 0)
+							{
+								col--;
+							}
+							else
+							{
+								col++;
+							}
+							if (col == toCol)
+							{
+								return true;
+							}
+							if (board[fromRow * 8 + col] != piece.NA)
+							{
+								return false;
+							}
+						}
+					}
+				case piece.BQ:
+				case piece.WQ:
+					if (diagonal)
+					{
+						for (int row = fromRow, col = fromCol; ; )
+						{
+							if (toRow - fromRow < 0)
+							{
+								row--;
+							}
+							else
+							{
+								row++;
+							}
+							if (toCol - fromCol < 0)
+							{
+								col--;
+							}
+							else
+							{
+								col++;
+							}
+							if (row == toRow || col == toCol)
+							{
+								return true;
+							}
+							if (board[row * 8 + col] != piece.NA)
+							{
+								return false;
+							}
+						}
+					}
+					else if (vertical)
+					{
+						for (int row = fromRow; ; )
+						{
+							if (toRow - fromRow < 0)
+							{
+								row--;
+							}
+							else
+							{
+								row++;
+							}
+							if (row == toRow)
+							{
+								return true;
+							}
+							if (board[row * 8 + fromCol] != piece.NA)
+							{
+								return false;
+							}
+						}
+					}
+					else
+					{
+						for (int col = fromCol; ; )
+						{
+							if (toCol - fromCol < 0)
+							{
+								col--;
+							}
+							else
+							{
+								col++;
+							}
+							if (col == toCol)
+							{
+								return true;
+							}
+							if (board[fromRow * 8 + col] != piece.NA)
+							{
+								return false;
+							}
+						}
+					}
+				case piece.BP:
+				case piece.WP:
+					for (int row = fromRow; ; )
+					{
+						if (toRow - fromRow < 0)
+						{
+							row--;
+						}
+						else
+						{
+							row++;
+						}
+						if (board[row * 8 + fromCol] != piece.NA)
+						{
+							return false;
+						}
+						if (row == toRow)
+						{
+							return true;
+						}
+					}
+				default:
+					return false;
+			}
+		}
+
+		private bool Movable(int from, int to)
+		{
+			int fromRow = from / 8, fromCol = from % 8, toRow = to / 8, toCol = to % 8;
+			bool diagonal = Math.Abs(fromRow - toRow) == Math.Abs(fromCol - toCol);
+			bool vertical = (fromCol - toCol) == 0;
+			bool horizontal = (fromRow - toRow) == 0;
+			bool adjacent = Math.Abs(fromRow - toRow) <= 1 && Math.Abs(fromCol - toCol) <= 1;
+			bool horse = Math.Abs(fromRow - toRow) > 0 && Math.Abs(fromCol - toCol) > 0 && (Math.Abs(fromRow - toRow) + Math.Abs(fromCol - toCol) == 3);
+			bool pawn = ((fromRow == 1 || fromRow == 6) && vertical && Math.Abs(fromRow - toRow) <= 2) || (vertical && Math.Abs(fromRow - toRow) == 1);
+			bool pawnEat = Math.Abs(fromRow - toRow) == 1 && Math.Abs(fromCol - toCol) == 1;
+			switch (board[from])
+			{
+				case piece.BK:
+				case piece.WK:
+					return adjacent;
+				case piece.BB:
+				case piece.WB:
+					if (diagonal)
+					{
+						return isNotBlock(from, to, fromRow, fromCol, toRow, toCol, vertical, horizontal, diagonal);
+					}
+					else
+					{
+						return false;
+					}
+				case piece.BQ:
+				case piece.WQ:
+					if (diagonal || vertical || horizontal)
+					{
+						return isNotBlock(from, to, fromRow, fromCol, toRow, toCol, vertical, horizontal, diagonal);
+					}
+					else
+					{
+						return false;
+					}
+				case piece.BR:
+				case piece.WR:
+					if (vertical || horizontal)
+					{
+						return isNotBlock(from, to, fromRow, fromCol, toRow, toCol, vertical, horizontal, diagonal);
+					}
+					else
+					{
+						return false;
+					}
+				case piece.BN:
+				case piece.WN:
+					return horse;
+				case piece.BP:
+					if (pawn && fromRow - toRow < 0)
+					{
+						return isNotBlock(from, to, fromRow, fromCol, toRow, toCol, vertical, horizontal, diagonal);
+					}
+					else if (pawnEat && board[to] != piece.NA && fromRow - toRow < 0)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				case piece.WP:
+					if (pawn && fromRow - toRow > 0)
+					{
+						return isNotBlock(from, to, fromRow, fromCol, toRow, toCol, vertical, horizontal, diagonal);
+					}
+					else if (pawnEat && board[to] != piece.NA && fromRow - toRow > 0)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				default:
+					return false;
+			}
+		}
+
+		private bool isSameColor(int a,int b)
+		{
+			if ((int)board[a] == 12 || (int)board[b] == 12)
+			{
+				return false;
+			}
+			else if (((int)board[a] + (int)board[b]) % 2 == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private void GameSet()
+		{
+			board[0] = piece.BR;
+			board[1] = piece.BN;
+			board[2] = piece.BB;
+			board[3] = piece.BQ;
+			board[4] = piece.BK;
+			board[5] = piece.BB;
+			board[6] = piece.BN;
+			board[7] = piece.BR;
+			for (int i = 8; i < 16; i++)
+			{
+				board[i] = piece.BP;
+			}
+			for (int i = 16; i < 48; i++)
+			{
+				board[i] = piece.NA;
+			}
+			for (int i = 48; i < 56; i++)
+			{
+				board[i] = piece.WP;
+			}
+			board[56] = piece.WR;
+			board[57] = piece.WN;
+			board[58] = piece.WB;
+			board[59] = piece.WQ;
+			board[60] = piece.WK;
+			board[61] = piece.WB;
+			board[62] = piece.WN;
+			board[63] = piece.WR;
+			checkIndex = -1;
+		}
+
+		private void RefreshBoard()
+		{
+			for (int i = 0; i < 64; i++)
+			{
+				square[i].Checked = false;
+				switch (board[i])
+				{
+					case piece.WR:
+						square[i].ImageKey = "WhiteRook.png";
+						break;
+					case piece.WQ:
+						square[i].ImageKey = "WhiteQueen.png";
+						break;
+					case piece.WP:
+						square[i].ImageKey = "WhitePawn.png";
+						break;
+					case piece.WN:
+						square[i].ImageKey = "WhiteKnight.png";
+						break;
+					case piece.WK:
+						square[i].ImageKey = "WhiteKing.png";
+						break;
+					case piece.WB:
+						square[i].ImageKey = "WhiteBishop.png";
+						break;
+					case piece.BR:
+						square[i].ImageKey = "BlackRook.png";
+						break;
+					case piece.BQ:
+						square[i].ImageKey = "BlackQueen.png";
+						break;
+					case piece.BP:
+						square[i].ImageKey = "BlackPawn.png";
+						break;
+					case piece.BN:
+						square[i].ImageKey = "BlackKnight.png";
+						break;
+					case piece.BK:
+						square[i].ImageKey = "BlackKing.png";
+						break;
+					case piece.BB:
+						square[i].ImageKey = "BlackBishop.png";
+						break;
+					default:
+						square[i].ImageKey = "";
+						break;
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			white = black = 1800;
+			now = player.WHITE;
+			whiteTurn.Visible = true;
+			for (int i = 0; i < 64; i++)
+			{
+				square[i].Enabled = true;
+			}
+			timer1.Start();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (now == player.WHITE)
+			{
+				white--;
+				whiteClock.Text = "" + white / 60 + ":" + white % 60;
+			}
+			else
+			{
+				black--;
+				blackClock.Text = "" + black / 60 + ":" + black % 60;
+			}
+		}
+	}
+}
